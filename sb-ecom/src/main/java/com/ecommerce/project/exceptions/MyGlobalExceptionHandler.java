82af742603200768145aa83732815fe6ceeb0e2d
shiwani77177 <shiwanisinha77177@gmail.com>
2026-08-10 20:51:13 +0530

    Force LF on mvnw so the Docker build can exec it

--- sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.ecommerce.project.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class MyGlobalExceptionHandler {
