9c5671dac509624d64ed7a2f06f74b959b177d26
Shiwani <shiwanisinha77177@gmail.com>
2026-08-11 17:29:32 +0530

    Add files via upload

--- sb-ecom/src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -4,5 +4,7 @@ import com.ecommerce.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    public Category findByCategoryName(String categoryName);
 }
 
